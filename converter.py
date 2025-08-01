1bb8f8700abe7977c505ccc7092adbbcf6333ab6
yuandev-pixel <niannian2024@yeah.net>
2025-08-01 17:15:54 +0800

    test

---
+++ converter.py
@@ -0,0 +1,61 @@
+L=[]
+with open("cube.obj","r") as file:
+	lines = file.readlines()
+	vf=True
+	ff=True
+	count=0
+	rc=0
+	for line in lines:
+		if line[0:2]=="v ":
+			if vf:
+				L.append("---vertstart---\n")
+				count+=1
+				vf=False
+			temp=line.split(" ")
+			temp[3]=temp[3].strip()
+			L.append("v "+str(float(temp[1])*100)+" "+str(float(temp[2])*100)+" "+str(float(temp[3])*100+300)+" 0 0 0\n")
+			count+=1
+		if line[0:2]=="f ":
+			if ff:
+				L.append("---facestart---\n")
+				count+=1
+				ff=False
+			temp=line.split(" ")
+			# print(temp)
+			print(line)
+
+			tt=[]
+			# print(temp)
+			# print(len(temp))
+			if len(temp)>4:
+				targ="f "
+				targ+=str(temp[1].strip())+" "
+				for i in range(3,len(temp)-1):
+					targ+=str(temp[i].strip())+" "
+				targ+=str(temp[len(temp)-1].strip())
+				targ+="\n"
+				# print(targ)
+			# 	# print(targ)
+				lines.insert(rc+1,targ)
+				rc+=1
+
+			print(lines)
+			cnt=0
+			for vert in temp:
+				if cnt>0 and cnt<4:
+					tt.append(vert.split("/")[0])
+				cnt+=1
+			L.append("f")
+			# print(count)
+			for item in tt:
+				L[count]=L[count]+" "+str(item)
+			L[count]=L[count]+"\n"
+			# count+=1
+			# if len(temp)>4:
+				# count+=1
+			count+=1
+		rc+=1
+	L.append("---textstart---")
+print(L)
+with open("stupid.mystupidfile","w") as file:
+	file.writelines(L)
